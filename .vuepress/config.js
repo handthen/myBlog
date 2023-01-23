37c1cf27605c7a047fe47b4e0ce6ea2b9d38fd85
1470607843@qq.com <1470607843@qq.com>
2023-01-23 15:58:14 +0800

    初始化

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: "MY BLOG",
+    description: "",
+    themeConfig: {
+        nav: [
+            {
+                text: "首页",
+                link: "/"
+            },
+            {
+                text:"my blog",
+                items:[
+                    { text: "掘金", link: "https://juejin.cn/user/233526039432445" },
+                ]
+            }
+        ]
+    }
+}
